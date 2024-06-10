66465556e1e1fb58622979a260957d3d5a97c0ed
Vandan Shah <veer@WINDOWS-51MVSMG.localdomain>
2024-06-10 12:46:28 -0400

    changed url

--- vite.config.js
+++ vite.config.js
@@ -8,7 +8,7 @@ export default defineConfig({
   plugins: [
     vue(),
   ],
-  base: "/element-data-sharing",
+  base: "/",
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url))
