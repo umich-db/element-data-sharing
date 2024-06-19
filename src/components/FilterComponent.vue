0b779c78dcb239e2869671c95fc22b484985a589
yangjus <justiny_2013@yahoo.com>
2024-06-18 20:25:00 -0500

    search bar element

--- src/components/FilterComponent.vue
+++ src/components/FilterComponent.vue
@@ -1,6 +1,6 @@
 <script setup>
 import { ref } from "vue"
-import SelectLanguage from "./SelectLanguage.vue"
+import SelectLanguage from "./elements/SelectLanguage.vue"
 defineProps({
   title: {
     type: String,
