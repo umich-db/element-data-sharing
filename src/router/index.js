7891d901f2dab4e8740d2d03972fed3ea2c3cda9
Eric Guo <ericguo@EricdeMacBook-Pro-2.local>
2024-07-25 13:34:09 -0400

    all route fixed

--- src/router/index.js
+++ src/router/index.js
@@ -1,4 +1,4 @@
-import { createRouter, createWebHistory } from 'vue-router';
+import { createRouter, createWebHashHistory } from 'vue-router';
 import DetailedInfo from '@/components/DetailedInfo.vue';
 import SearchComponent from '@/components/SearchComponent.vue';
 
@@ -16,7 +16,7 @@ const routes = [
 ];
 
 const router = createRouter({
-  history: createWebHistory(),
+  history: createWebHashHistory(),
   routes,
 });
 
